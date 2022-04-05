3c18adf560093e74a1e08a85c592e4df29d7cce5
Your Name <you@example.com>
2022-04-05 11:07:22 +0800

    second

---
+++ src/main/java/vttp/paf/day2/giphy_workflow/GiphyWorkflowApplication.java
@@ -0,0 +1,13 @@
+package vttp.paf.day2.giphy_workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiphyWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiphyWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp/paf/day2/giphy_workflow/GiphyWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.paf.day2.giphy_workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiphyWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
